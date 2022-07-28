a79554dbdd55a0e1009954b22ca01ed5d268b386
MALEK-FRAIJ <malekfraij21@gmail.com>
2022-07-28 21:13:52 +0300

    Add a solution to a question A. Dice Tower

---
+++ DiceTower.java
@@ -0,0 +1,40 @@
+
+//package CodeForces;
+import java.util.Scanner;
+
+/**
+ * Dice-Tower
+ */
+
+/* A. Dice Tower (1) */
+
+public class DiceTower {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int numberOfDice, topOfTower, leftInterface, rightInterface;
+
+        numberOfDice = scan.nextInt();
+        topOfTower = scan.nextInt();
+        int count = 1;
+
+        for (int i = 0; i < numberOfDice; i++) {
+
+            leftInterface = scan.nextInt();
+            rightInterface = scan.nextInt();
+
+            if (topOfTower == rightInterface || topOfTower == leftInterface ||
+                    topOfTower == 7 - rightInterface || topOfTower == 7 - leftInterface) {
+
+                count = 0;
+
+            }
+            topOfTower = 7 - topOfTower;
+        }
+
+        System.out.println((count == 1 ? "YES" : "NO"));
+
+    }
+}
