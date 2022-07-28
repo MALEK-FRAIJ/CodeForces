090c0f01620669084b618525881b2604f8944aae
MALEK-FRAIJ <malekfraij21@gmail.com>
2022-07-28 23:54:41 +0300

    Add a solution to a question A. Die Roll

---
+++ DieRoll.java
@@ -0,0 +1,17 @@
+
+//package CodeForces;
+import java.util.Scanner;
+
+/**
+ * DieRoll
+ */
+
+/* A. Die Roll (2) */
+public class DieRoll {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
