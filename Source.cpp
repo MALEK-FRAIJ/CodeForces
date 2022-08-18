cb62a066c3e14be57720d1e471fcc92d954c6488
MALEK-FRAIJ <malekfraij21@gmail.com>
2022-08-18 16:57:35 +0300

    Add a solution to the problem using c++'

---
+++ Source.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(void) {
+
+/*H. Two numbers
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output*/
+	
+	double a, b;
+	cin >> a >> b;
+
+	cout << "floor " << a << " / " << b << " = " << floor(a / b) << endl;
+	cout << "ceil " << a << " / " << b << " = " << ceil(a / b) << endl;
+	cout << "round " << a << " / " << b << " = " << round(a / b) << endl;
+	return 0;
+}
