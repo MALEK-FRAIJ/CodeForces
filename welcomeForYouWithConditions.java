5fcc6cebaf99ffb7bf9df013e1b077cae27aba9f
MALEK-FRAIJ <malekfraij21@gmail.com>
2022-08-18 14:52:26 +0300

    Add a solution to a question Welcome for you with Conditions

---
+++ welcomeForYouWithConditions.java
@@ -0,0 +1,28 @@
+//package CodeForces;
+
+import java.util.Scanner;
+
+/* Welcome for you with Conditions*/
+public class welcomeForYouWithConditions {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Only one line containing two numbers A and B (0 ≤ A, B ≤ 100).
+        byte number1, number2;
+
+        number1 = scan.nextByte();
+        number2 = scan.nextByte();
+
+        // Print "Yes" if A is greater than or equal to B.
+        if (number1 >= number2) {
+
+            System.out.println("Yes");
+
+            // Otherwise print "No".
+        } else {
+            System.out.println("No");
+        }
+
+    }
+}
