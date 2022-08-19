d6f3026af6b29456c76055a0c7f4fa41f53a8c7d
MALEK-FRAIJ <malekfraij21@gmail.com>
2022-08-19 15:01:55 +0300

    Add a solution to the problem

---
+++ Char.java
@@ -0,0 +1,25 @@
+//package CodeForces;
+
+import java.util.Scanner;
+/* N. Char
+ time limit per test0.25 seconds
+memory limit per test64 megabytes
+input standard input
+output standard output
+ */
+
+//Note : difference between 'a' and 'A' in ASCII is 32 .
+public class Char {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        char x = scan.next().charAt(0);
+
+        if (x > 64 && x < 91) {
+            System.out.println((char) (x + 32));
+        } else {
+            System.out.println((char) (x - 32));
+        }
+
+    }
+}
---
+++ FirstDigit.java
@@ -0,0 +1,28 @@
+//package CodeForces;
+
+import java.util.Scanner;
+
+/* P. First digit !
+   time limit per test0.25 seconds
+   memory limit per test64 megabytes
+   input standard input
+   output standard output
+    */
+
+public class FirstDigit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt(); // number X (999 < X  ≤  9999)
+
+        while (number >= 10) 
+            number /= 10;
+       
+        //If the first digit is even print "EVEN" otherwise print "ODD".
+        if (number % 2 == 0)
+            System.out.println("EVEN");
+        else
+            System.out.println("ODD");
+
+    }
+}
